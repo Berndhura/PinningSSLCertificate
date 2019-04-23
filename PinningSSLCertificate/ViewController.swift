289987d6a7ab9db721f59a2449d786ca6301bc92
wichura@gmx.de <Bw12345!>
2019-04-23 22:07:00 +0200

    add apple certificate to project and pin it

--- PinningSSLCertificate/ViewController.swift
+++ PinningSSLCertificate/ViewController.swift
@@ -16,16 +16,48 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var webView: WKWebView!
     
+    private var sessionManager: SessionManager?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         let url = URL(string: "https://www.apple.com")!
+        
+        enableCertificatePinning()
+        
+        sessionManager?.request(url).responseData { result in
+        
+        }
+        
         Alamofire.request(url).responseData { result in
             if let data = result.data, let requestURL = result.request?.url {
                 self.webView.load(data, mimeType: result.response?.mimeType ?? "", characterEncodingName: result.response?.textEncodingName ?? "", baseURL: requestURL)
             }
         }
+    }
+    
+    private func enableCertificatePinning() {
+        //let certificates: [SecCertificate] = []
+        let certificates = getCertificates()
+        let trustPolicy = ServerTrustPolicy.pinCertificates(
+            certificates: certificates,
+            validateCertificateChain: true,
+            validateHost: true)
+        let trustPolicies = [ "www.apple.com": trustPolicy ]
+        let policyManager =  ServerTrustPolicyManager(policies: trustPolicies)
+        sessionManager = SessionManager(
+            configuration: .default,
+            serverTrustPolicyManager: policyManager)
+    }
+    
+    private func getCertificates() -> [SecCertificate] {
+        let url = Bundle.main.url(forResource: "appleCert", withExtension: "cer")!
+        let localCertificate = try! Data(contentsOf: url) as CFData
+        guard let certificate = SecCertificateCreateWithData(nil, localCertificate)
+            else { return [] }
+        
+        return [certificate]
     }
 }
 
